b7770462b4710bafc82261e3b80064506eabcad2
Kurin, Alex <olexkurin@gmail.com>
2022-04-05 11:53:04 +0300

    first commit

---
+++ Programming-1-Course-Work/MyForm.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+#include "MyForm.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    System::Windows::Forms::Application::EnableVisualStyles();
+    System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
+    System::Windows::Forms::Application::Run(gcnew Programming1CourseWork::MyForm);
+    return 0;
+}
---
+++ Programming-1-Course-Work/MyForm.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace Programming1CourseWork
+{
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm()
+		{
+			InitializeComponent();
+		}
+
+	protected:
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"MyForm";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
